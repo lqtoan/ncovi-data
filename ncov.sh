3a0cd2084b6e8813d67c1b61e965d4e78a1a8cb3
Lê Quốc Toàn <89382043+lqtoan@users.noreply.github.com>
2021-11-28 15:29:12 +0700

    upload file

---
+++ ncov.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+
+# [ SEUID -eq 0 ] && echo "Do not run this script as root." && exit && exit 1
+
+country=$1
+[ -z $country ] && country="Vietnam"
+# file="$HOME/covid.log"
+
+curl -s https://ncovi.vnpt.vn/thongtindichbenh_v2 | awk -F'[{|}]' '{ for (i = 1; i < 5000; ++i) print $i;}' | grep $country | awk -F',' '{ for (i = 1; i < 11; ++i) print $i;}'
+
+exit 0
